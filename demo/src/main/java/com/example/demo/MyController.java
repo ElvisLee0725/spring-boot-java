f80b6e7c6abb60b9fbcd3fdeb21b29117d88bd92
ElvisLee0725 <elvislee0725@gmail.com>
2021-07-14 11:01:27 -0700

    4 ways to retrieve from url

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/detail")
+import java.util.ArrayList;
+import java.util.List;
+
+// @RequestMapping("/detail")
 @RestController
 public class MyController {
 
@@ -18,16 +20,49 @@ public class MyController {
 //        printer.print("Hello Big World");
 //        return "Hello World";
 //    }
+    @RequestMapping("/test1")
+    public String test1(@RequestParam Integer id,
+                        @RequestParam(defaultValue = "Amy") String name) {
+        System.out.println("Id: " + id);
+        System.out.println(("Name: " + name));
+        return "test1 query parameter";
+    }
+
+    @RequestMapping("/test2")
+    public String test2(@RequestBody Student student) {
+        System.out.println("Id: " + student.getId());
+        System.out.println(("Name: " + student .getName()));
+        return "test2 request body";
+    }
+
+    @RequestMapping("/test3")
+    public String test3(@RequestHeader(name = "Content-Type") String contentType) {
+        System.out.println("Content-Type: " + contentType);
+        return "test3 request header";
+    }
+
+    @RequestMapping("/test4/{id}/{name}")
+    public String test4(@PathVariable Integer id,
+                        @PathVariable String name) {
+        System.out.println("Id: " + id);
+        System.out.println("Name: " + name);
+        return "test4 path variable";
+    }
 
     @RequestMapping("/product")
-    public String product() {
-        System.out.printf("Call product path");
-        return "First is apple, second is orange";
+    public Store product() {
+        Store store = new Store();
+        List<String> list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Orange");
+        store.setProductList(list);
+        return store;
     }
 
     @RequestMapping("/user")
-    public String user() {
-        System.out.println("Call user path");
-        return "Elvis Lee";
+    public Student user() {
+        Student s = new Student();
+        s.setName("Elvis Lee");
+        return s;
     }
 }
--- demo/src/main/java/com/example/demo/Student.java
+++ demo/src/main/java/com/example/demo/Student.java
@@ -3,11 +3,22 @@ package com.example.demo;
 import java.util.List;
 
 public class Student {
-    Integer id;
     String name;
-    Double score;
-    Boolean graduated;
-    List<String> courseList;
-    Pet pet;
-    List<Pet> petList;
+    Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
