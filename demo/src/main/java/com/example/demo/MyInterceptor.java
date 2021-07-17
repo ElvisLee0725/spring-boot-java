07db901cfc3297528eb3d9b9d6aa3b78fdf0d802
ElvisLee0725 <elvislee0725@gmail.com>
2021-07-17 00:25:18 -0700

    Added JDBC to RESTful API for update()

--- demo/src/main/java/com/example/demo/MyInterceptor.java
+++ demo/src/main/java/com/example/demo/MyInterceptor.java
@@ -11,8 +11,7 @@ public class MyInterceptor implements HandlerInterceptor {
     @Override
     // If return true, the request is accepted; If return false, the request is blocked
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("Execute prehandle()");
         response.setStatus(401);
-        return false;
+        return true;
     }
 }
--- demo/src/main/java/com/example/demo/StudentController.java
+++ demo/src/main/java/com/example/demo/StudentController.java
@@ -1,21 +1,41 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
+import java.util.HashMap;
+import java.util.Map;
 
 // Implement Student API
 @RestController
 @Validated
 public class StudentController {
 
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
     // Create student
     @PostMapping("/students")
-    //@RequestMapping(value = "/students", method = RequestMethod.POST)
-    public String create(@RequestBody @Valid Student student) {
-        return "Execute create operation";
+    public String insert(@RequestBody Student student) {
+        String sql = "INSERT INTO student(id, name) VALUE (:studentId, :studentName)";
+        Map<String, Object> map = new HashMap<>();
+        map.put("studentId", student.getId());
+        map.put("studentName", student.getName());
+
+        namedParameterJdbcTemplate.update(sql, map);
+        return "Execute INSERT sql";
+    }
+
+    @DeleteMapping("/students/{studentId}")
+    public String delete(@PathVariable Integer studentId) {
+        String sql = "DELETE FROM student WHERE id = :studentId";
+        Map<String, Object> map = new HashMap<>();
+        map.put("studentId", studentId);
+        namedParameterJdbcTemplate.update(sql, map);
+        return "Execute DELETE sql";
     }
 
     // Read student with id
@@ -29,10 +49,5 @@ public class StudentController {
     public String update(@PathVariable Integer studentId,
                          @RequestBody Student student) {
         return "Execute update operation";
-    }
-
-    @DeleteMapping("/students/{studentId}")
-    public String delete(@PathVariable Integer studentId) {
-        return "Execute delete operation";
     }
 }
